72760abcdf40f8eb0d1857cd20217cce288157d3
Sachin I <sachin.isaiah04@gmail.com>
2026-09-08 12:46:56 +0530

    Connect SortSense to Aiven MySQL

--- app.py
+++ app.py
@@ -203,7 +203,8 @@ def get_db_connection():
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
             database=os.getenv("DB_NAME"),
-            port=int(os.getenv("DB_PORT", 3306))
+            port=int(os.getenv("DB_PORT", 3306)),
+            ssl_ca=os.getenv("DB_SSL_CA")
         )
 
         return connection
