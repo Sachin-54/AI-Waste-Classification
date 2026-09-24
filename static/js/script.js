519c401f325755edc8b3c6daeb0230eb6a1205e4
Sachin I <sachin.isaiah04@gmail.com>
2026-09-24 11:19:51 +0530

    Fix prediction endpoint for deployment

--- static/js/script.js
+++ static/js/script.js
@@ -403,7 +403,7 @@ if (predictBtn) {
             // SEND IMAGE TO FLASK
             // ===============================
 
-            const response = await fetch("http://127.0.0.1:5000/predict", {
+            const response = await fetch("/predict", {
                 method: "POST",
                 body: formData
             });
